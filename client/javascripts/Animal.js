cecf2f0a2c220724c41b5746eaf94dd61acca428
Judy Lam <judyl@uoregon.edu>
2020-05-24 03:07:43 -0700

    Animal.js

--- client/javascripts/Animal.js
+++ client/javascripts/Animal.js
@@ -47,8 +47,19 @@ class Rabbit extends Animal {
 
 // Inherit from Animal
 class Dog extends Animal {
-  //your code goes here..
-
+  constructor(name, breed, weight, barking) {
+    super(name);
+    this.breed = breed;
+    this.weight = weight;
+    this.barking = barking;
+  }
+bark() {
+  return `${this.barking} ${this.barking}!`;
+}
+stop() {
+  super.stop();
+  console.log(this.bark());
+  }
 }
 
 let test_driver = () => {
@@ -60,7 +71,7 @@ let test_driver = () => {
     rabbit.stop();
     console.log(rabbit.name);
     console.log(rabbit.earLength);
-  }
+  };
 
   let test_dog = () => {
     let fang = new Dog("Fang", "Airedale", 50, "Woof");
@@ -74,9 +85,9 @@ let test_driver = () => {
     console.log(`Look, a cat! ${zero.name} barks: ${zero.bark()}`);
     zero.stop();
 
-  }
+  };
 
   //run tests
   test_rabbit();
   test_dog();
-}
+};
